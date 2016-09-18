35a57e62a8324cea9af308af57ec5ba29cda460a
Willbo <willbroderick@gmail.com>
2016-09-18 17:16:58 +0100

    Starting

--- db/migrate/20160918085245_add_value_types_to_topi_c_rules.rb
+++ db/migrate/20160918085245_add_value_types_to_topi_c_rules.rb
@@ -1,7 +1,7 @@
 class AddValueTypesToTopiCRules < ActiveRecord::Migration
   def change
     change_table :topic_rules do |t|
-      t.remove :value, :string
+      t.remove :value
       t.integer :value_integer
       t.datetime :value_datetime
       t.string :value_string

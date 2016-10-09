8e35ee82aafae5e89528efc4a8d69369e3dc7be5
Willbo <willbroderick@gmail.com>
2016-10-09 11:23:32 +0100

    Puma update

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,8 @@ threads 1, 4
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
+preload_app!
+
 # Default to production
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
@@ -23,7 +25,11 @@ state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
 end

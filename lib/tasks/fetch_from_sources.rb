a8b67eb73434909a7858429e881c8ef7745e5891
Willbo <willbroderick@gmail.com>
2016-10-22 16:45:06 +0100

    Using delayed job instead of cron

--- lib/tasks/fetch_from_sources.rb
+++ lib/tasks/fetch_from_sources.rb
@@ -1,16 +1,16 @@
-class FetchFromSources
-  include Delayed::RecurringJob
-  run_every 1.day
-  run_at { ['06:00','10:00','16:00','22:00'] }
-  queue 'slow-jobs'
+# class FetchFromSources
+#   include Delayed::RecurringJob
+#   run_every 1.day
+#   run_at { ['06:00','10:00','16:00','22:00'] }
+#   queue 'slow-jobs'
 
-  def perform
-    @total = 0
-    ms = Benchmark.ms do
-      Source.where(enabled: true).each do |source|
-        @total = @total + source.fetch
-      end
-    end
-    puts "Articles fetched: #{@total} in #{(ms/1000).to_i}s"
-  end
-end
+#   def perform
+#     @total = 0
+#     ms = Benchmark.ms do
+#       Source.where(enabled: true).each do |source|
+#         @total = @total + source.fetch
+#       end
+#     end
+#     puts "Articles fetched: #{@total} in #{(ms/1000).to_i}s"
+#   end
+# end
